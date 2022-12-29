c160aa1393a4a2b2a054238bad2ff2022ac2d90a
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-12-29 12:28:54 -0800

    debug

--- app/server.c
+++ app/server.c
@@ -8,121 +8,138 @@
 #include <unistd.h>
 #include <sys/select.h>
 
-# define PORT 6379
+#define PORT 6379
 
-int bind_and_listen(struct sockaddr *client_addr, int* client_addr_len, int port) {
+int bind_and_listen(struct sockaddr *client_addr, int *client_addr_len, int port)
+{
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
+	if (server_fd == -1)
+	{
 		printf("Socket creation failed: %s...\n", strerror(errno));
 		return -1;
 	}
-	
+
 	// Since the tester restarts your program quite often, setting REUSE_PORT
 	// ensures that we don't run into 'Address already in use' errors
 	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
+	{
 		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
 		return -1;
 	}
-	
-	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-									 .sin_port = htons(port),
-									 .sin_addr = { htonl(INADDR_ANY) },
-									};
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+
+	struct sockaddr_in serv_addr = {
+			.sin_family = AF_INET,
+			.sin_port = htons(port),
+			.sin_addr = {htonl(INADDR_ANY)},
+	};
+	if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0)
+	{
 		printf("Bind failed: %s \n", strerror(errno));
 		return -1;
 	}
-	
+
 	int connection_backlog = 0;
-	if (listen(server_fd, connection_backlog) != 0) {
+	if (listen(server_fd, connection_backlog) != 0)
+	{
 		printf("Listen failed: %s \n", strerror(errno));
 		return -1;
 	}
-	
-	client_addr_len = (int *) sizeof(client_addr);
+
+	client_addr_len = (int *)sizeof(client_addr);
 	return server_fd;
 }
 
+void handle_connection(int conn, fd_set *__restrict current_sockets)
+{
+	unsigned char *ping = "PING\r\n";
+	unsigned char *PONG = "+PONG\r\n";
+	unsigned char buf[6] = {0};
 
-void handle_connection(int conn, fd_set * __restrict current_sockets) {
-	unsigned char* ping = "PING\r\n";
-	unsigned char* PONG = "+PONG\r\n";
-	unsigned char buf[6] = { 0 };
-
-	if (recv(conn, buf, 6, 0) > 0) {
+	if (recv(conn, buf, 6, 0) > 0)
+	{
 		write(conn, PONG, strlen(PONG));
-	} else {
+	}
+	else
+	{
 		FD_CLR(conn, current_sockets);
 	}
 }
 
-
 // IO Multiplexing (One thread listens to multiple sockets!)
-void run_multiplex(const int server_socket, struct sockaddr_in* client_addr, int client_addr_len) {
+void run_multiplex(int server_socket, struct sockaddr_in *client_addr, int client_addr_len)
+{
+	// Initialize current file descriptor set and add server socket into our fdset
 	fd_set current_sockets, ready_sockets;
-	// Initialize current file descriptor set
 	FD_ZERO(&current_sockets);
-	// Add server socket into our fdset
 	FD_SET(server_socket, &current_sockets);
 
-	// make a copy
+	// make a copy of server socket, this is because for some weird reason server_socket gets changed
 	int sv = server_socket;
-	
-	while (1) {
-		// printf("Server Socket Open at FD [%d]\n", sv);
-		// printf("Waiting for a client to connect or socket to be read from!...\n");
+
+	while (1)
+	{
+		printf("Waiting for a client to connect or socket to be read from!...\n");
 		// make a copy because select is destructive
 		ready_sockets = current_sockets;
+
 		// select() returns the number of ready descriptors that are contained in the descriptor sets, or -1 if an error occurred.
-		
 		int numready = select(FD_SETSIZE, &ready_sockets, NULL, NULL, NULL);
-		
-		if (numready < 0) {
+
+		if (numready < 0)
+		{
 			perror("Select error");
 			exit(1);
 		}
 
-		if (numready == 0) {
+		if (numready == 0)
+		{
 			continue;
 		}
 
 		// Iterate over entire fdset, if the socket is ready.
 		// Ready socket is actually an incoming message on an existing client socket.
-		for (int i = 0; i < FD_SETSIZE; i++) {
-			if (!FD_ISSET(i, &ready_sockets)) {
+		for (int i = 0; i < FD_SETSIZE; i++)
+		{
+			if (!FD_ISSET(i, &ready_sockets))
+			{
 				continue;
 			}
 
-			if (i == sv) {
+			if (i == sv)
+			{
 				int cpsv = sv;
-				int new_client_socket = accept(cpsv, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-				if (new_client_socket < 0) {
+				int new_client_socket = accept(cpsv, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+				if (new_client_socket < 0)
+				{
 					perror("Error accepting new client connection");
 					continue;
 				}
-
+				printf("New connection accepted. \n");
 				FD_SET(new_client_socket, &current_sockets);
-			} else {
+			}
+			else
+			{
 				handle_connection(i, &current_sockets);
 			}
 		}
 	}
 }
 
-
-int main() {
+int main()
+{
 	// Disable output buffering
 	setbuf(stdout, NULL);
 
 	int server_socket, client_addr_len;
 	struct sockaddr_in client_addr;
 
-	server_socket = bind_and_listen((struct sockaddr *) &client_addr, &client_addr_len, PORT);
-	if (server_socket < 0) {
+	server_socket = bind_and_listen((struct sockaddr *)&client_addr, &client_addr_len, PORT);
+	if (server_socket < 0)
+	{
 		return 1;
 	}
-	
+
 	run_multiplex(server_socket, &client_addr, client_addr_len);
 
 	close(server_socket);
